62c0cdd669cd79ee4f914b62ba780d341d9817d5
Henrique Marlon <henriquemarloncs@gmail.com>
2024-08-29 14:16:13 -0300

    feat: new token USDC

--- contracts/script/DeployTokens.s.sol
+++ contracts/script/DeployTokens.s.sol
@@ -5,15 +5,23 @@ pragma solidity ^0.8.20;
 import {Script} from "forge-std/Script.sol";
 import {console} from "forge-std/console.sol";
 import {Volt} from "@contracts/Volt.sol";
+import {USDC} from "@contracts/Stablecoin.sol";
 
-contract DeployVoltToken is Script {
+contract DeployTokens is Script {
     function run() external {
         vm.startBroadcast(vm.envUint("PRIVATE_KEY"));
         Volt volt = new Volt(vm.envAddress("INITIAL_ONWER"));
+        USDC usdc = new USDC();
         vm.stopBroadcast();
         console.log(
             "Volt address:",
             address(volt),
+            "deployed at:",
+            block.timestamp
+        );
+        console.log(
+            "USDC address:",
+            address(usdc),
             "deployed at:",
             block.timestamp
         );
